6af055d5b3e5d0bcd86cf59f697e5f385af89528
jmls9627 <jmls9627@gmail.com>
2020-06-10 16:04:33 -0400

    2.Components Reusability

--- 1.Basics/basics.jsx
+++ 1.Basics/basics.jsx
@@ -1,31 +1,35 @@
 function Button(props) {
-  //	const handleClick = () => setCounter(counter+1);
-    return (
-    <button onClick={props.onClickFunction}>
-       {props.message} + 1 = {props.message + 1}
-      </button>
-    ); 
-  }
-  
-  function Display(props){
-    return (
-      <div>{props.msg}</div>
-    );
-  }
-  
-  function App(){
-   const [counter,setCounter]=useState(0);
-   const IncrementCounter = () => setCounter(counter+1);   
-    return(
-     <div>
-       <Display msg="la suma es"/> 
-       <Button onClickFunction={IncrementCounter} message={counter}/>
-           
-     </div>
-    )
-  }
-  
-  ReactDOM.render(
-    <App/>,
-    document.getElementById('mountNode'),
+	const handleClick = () => props.onClickFunction(props.incre);
+  return (
+  <button onClick={handleClick}>   
+     +{props.incre} 
+    </button>
+  ); 
+}
+
+function Display(props){
+  return (
+    <div>{props.message}</div>
   );
+}
+
+function App(){
+ const [counter,setCounter]=useState(0);
+ const IncrementCounter = (increVal) => setCounter(counter+increVal);
+                              
+  return(
+   <div>
+      <Button onClickFunction={IncrementCounter} incre={1}/>
+      <Button onClickFunction={IncrementCounter} incre={5}/>
+      <Button onClickFunction={IncrementCounter} incre={10}/>
+      <Button onClickFunction={IncrementCounter} incre={100}/>
+      <Display  message={counter}/>
+      
+   </div>
+  )
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('mountNode'),
+);
