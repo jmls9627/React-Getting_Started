1a718db1b4970958d50d4cc8e1498216f0f5edf5
jmls9627 <jmls9627@gmail.com>
2020-06-10 16:38:45 -0400

    2.Tree Reconciliation in Action

--- 1.Basics/basics.jsx
+++ 1.Basics/basics.jsx
@@ -1,35 +1,24 @@
-function Button(props) {
-	const handleClick = () => props.onClickFunction(props.incre);
-  return (
-  <button onClick={handleClick}>   
-     +{props.incre} 
-    </button>
-  ); 
-}
+const render = () => {
+  //html 
+  document.getElementById('mountNode').innerHTML = `
+	<div>
+    Hello HTML
+    <input/>
+    <pre>${(new Date).toLocaleTimeString()}</pre>
+  </div>
+`;
 
-function Display(props){
-  return (
-    <div>{props.message}</div>
-  );
-}
-
-function App(){
- const [counter,setCounter]=useState(0);
- const IncrementCounter = (increVal) => setCounter(counter+increVal);
-                              
-  return(
-   <div>
-      <Button onClickFunction={IncrementCounter} incre={1}/>
-      <Button onClickFunction={IncrementCounter} incre={5}/>
-      <Button onClickFunction={IncrementCounter} incre={10}/>
-      <Button onClickFunction={IncrementCounter} incre={100}/>
-      <Display  message={counter}/>
-      
-   </div>
-  )
-}
-
+//React 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('mountNode'),
+  React.createElement(
+    'div', 
+    null, 
+    'Hello React',
+    React.createElement('input',null),
+    React.createElement('pre',null,(new Date).toLocaleTimeString()),
+  ),
+  document.getElementById('mountNode2'),
 );
+
+}
+  setInterval(render, 1000);
